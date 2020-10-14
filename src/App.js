0080e6c49dc90f6aff4324ca493461d76baec75b
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2020-10-14 13:39:32 +0545

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+	return (
+		<div className="bg-gray-500 px-16">
+			<div className="container mx-auto">
+				<h1 className="">Covid Procurement homepage</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
