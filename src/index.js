490eedce86f4730d043c6b3a4633159d2cd9f429
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2020-10-14 14:29:40 +0545

    CP-1 Navigation link added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/tailwind.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/tailwind.scss";
 import "./assets/main.scss";
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import {
+	Route,
+	NavLink,
+	BrowserRouter,
+	Switch,
+} from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import Notfound from "./components/notfound";
+import About from "./layouts/About";
+import Country from "./layouts/Country";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<div>
+				<ul>
+					<li>
+						<NavLink exact activeClassName="active" to="/">
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeClassName="active" to="/about">
+							About
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeClassName="active" to="/country/mexico">
+							Country
+						</NavLink>
+					</li>
+				</ul>
+				<hr />
+				<Switch>
+					<Route exact path="/" component={App} />
+					<Route path="/about" component={About} />
+					<Route path="/country/mexico" component={Country} />
+					<Route component={Notfound} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/layouts/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+	return (
+		<section>
+			<div>This is about page</div>
+		</section>
+	);
+}
+
+export default About;
