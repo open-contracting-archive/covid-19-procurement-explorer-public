d5837d9fc5704f7826fba86347d665ab1ad2f3ef
dhondup.lama <dhondup.lama@yipl.com.np>
2020-10-23 14:11:06 +0545

    build

---
+++ build/precache-manifest.1853c262d905795a5c032f5a0a3d9db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9673b39648ad7bcafed71adb5f5bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb8ca26e91c290e254c6",
+    "url": "/static/css/2.fd971cca.chunk.css"
+  },
+  {
+    "revision": "2894e463ae5a5c63fe28",
+    "url": "/static/css/main.1b5b47a1.chunk.css"
+  },
+  {
+    "revision": "cb8ca26e91c290e254c6",
+    "url": "/static/js/2.d41b3f0d.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.d41b3f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2894e463ae5a5c63fe28",
+    "url": "/static/js/main.14247541.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
