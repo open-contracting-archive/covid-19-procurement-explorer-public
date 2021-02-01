de9428890b16baf03144eb34c7e7cdc27cbb9b81
duptitung <dup.titung@gmail.com>
2021-02-01 17:10:55 +0545

    UI fixes

--- src/layouts/pages/StaticPage/index.js
+++ src/layouts/pages/StaticPage/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, Fragment } from "react"
+import React, { useState, useEffect, Fragment } from 'react'
 import { useParams, Redirect } from 'react-router-dom'
-import CmsPageService from "../../../services/CmsPageService"
-import Loader from "../../../components/Loader/Loader"
+import CmsPageService from '../../../services/CmsPageService'
+import Loader from '../../../components/Loader/Loader'
 
 const StaticPage = () => {
     let { slug } = useParams()
@@ -10,31 +10,37 @@ const StaticPage = () => {
     window.scrollTo(0, 0)
 
     useEffect(() => {
-        CmsPageService.StaticPageDetailBySlug(slug)
-            .then((response) => {
-                setPageDetail(response)
-                setLoading(false)
-            })
+        CmsPageService.StaticPageDetailBySlug(slug).then((response) => {
+            setPageDetail(response)
+            setLoading(false)
+        })
     }, [slug])
 
-    if (!loading && (pageDetail.title === undefined || pageDetail.title === null)) {
-        return (<Redirect to={"/" + slug} />)
+    if (
+        !loading &&
+        (pageDetail.title === undefined || pageDetail.title === null)
+    ) {
+        return <Redirect to={'/' + slug} />
     }
 
     return (
         <section className="pt-8">
             <div className="container mx-auto px-4 news-detail">
-                {loading ? (<Loader />) : (
+                {loading ? (
+                    <Loader />
+                ) : (
                     <Fragment>
                         <h2 className="md:w-3/4 text-lg md:text-xl leading-tight mb-6 md:mb-10 uppercase text-primary-dark">
                             {pageDetail.title}
                         </h2>
-                        <div className="flex flex-wrap lg:flex-no-wrap justify-between mb-10">
-                            <div className="mb-10 news-detail__content"
-                                 dangerouslySetInnerHTML={{
-                                     __html: pageDetail.rendered_body
-                                 }}>
-                            </div>
+                        <div
+                            style={{ minHeight: '40vh' }}
+                            className="flex flex-wrap lg:flex-no-wrap justify-between mb-10">
+                            <div
+                                className="mb-10 news-detail__content"
+                                dangerouslySetInnerHTML={{
+                                    __html: pageDetail.rendered_body
+                                }}></div>
                         </div>
                     </Fragment>
                 )}
